2e681e8446bf3ea299551113506cfaacaf85ead3
Tzachi Dar <tzachi.dar@gmail.com>
2016-07-06 01:52:21 +0300

    Fixes for the script.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- read_com9.py
+++ read_com9.py
@@ -1,19 +1,30 @@
 import serial
 import time
 import datetime
+import sys
 
-port_name = 'com9'
-file = open('%s_hist.txt' % port_name , 'a')
+if len(sys.argv) != 2:
+  print ("Need to supply port")
+  sys.exit()
+
+port_name = sys.argv[1]
+file = open('%s_hist.txt' % port_name , 'a', 1)
 
 
 ser = serial.Serial(port_name, 9600, timeout=0)
 while 1:
     try:
-        data = ser.read(ser.inWaiting()).decode("utf-8")
-
-        if not data:
+        bytes = ser.read(ser.inWaiting())
+        if len(bytes) < 1:
           time.sleep(50.0/1000.0)
           continue
+        print(bytes)
+        data = bytes.decode("ASCII")
+
+        #if not data:
+        #  time.sleep(50.0/1000.0)
+        #  continue
+        
         lines =data.splitlines()
         i = datetime.datetime.now()
         now = "%s" %i 
