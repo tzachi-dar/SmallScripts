179474169b398c23f4146591cb2a297a30a649bf
Tzachi Dar <tzachi.dar@gmail.com>
2016-12-29 21:29:46 +0200

    Also upload the data to mongo account.

--- read_com_port.py
+++ read_com_port.py
@@ -2,71 +2,84 @@ import serial
 import time
 import datetime
 import sys
-import re
-import subprocess
 
-# This function is needed for linux in order to convert names like
-# 02-5F-76-2F to names like /dev/ttyACM0
-# on windows it is not needed because a wixel will always be connected to
-# the same com port
-def get_com_from_name(serial):
+db_uri = 'mongodb://user:password@ds041673.mlab.com:41673'
+db_name = 'nightscout'
 
-    for i in range(0, 5):
-        device = '/dev/ttyACM%d' % i
-        #print device 
- 
-	process = subprocess.Popen(['udevadm', 'info', device], stdout=subprocess.PIPE,universal_newlines=True)
-	for line in iter(process.stdout.readline,''):
-            #print line.rstrip()
-	    cl = re.search(r'E: ID_SERIAL_SHORT=(.*)', line.rstrip())
-            if cl and cl.group(1) == serial:
-                print "found " +  cl.group(1)
-                return device
-    # We did not find such a device.
-    print 'Device %s not found.' % serial
-    print 'Example to device name is \'02-5F-76-2F\''
-    print 'run udevadm info /dev/ttyACM0 to find devices'
-    print 'Exiting'
-    sys.exit()
+from pymongo import MongoClient
+
+def create_object(wixel_data):
+    # an example to wixel_data is '6ABW4 54880 44800 213 -89 2'
+    mongo = dict()
+    data = wixel_data.split()
+    mongo['TransmitterId'] = data[0]
+    mongo['RawValue'] = int(data[1])
+    mongo['FilteredValue'] = int(data[2])
+    mongo['BatteryLife'] = int(data[3])
+    mongo['ReceivedSignalStrength'] = int(data[4])
+    mongo['TransmissionId'] = int(data[5])
+    mongo['UploaderBatteryLife'] = 100
+    captured_time = int(time.time() * 1000)
+    mongo['CaptureDateTime'] = captured_time
+    mongo['DebugInfo'] = 'pc %s' % time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(captured_time / 1000))
+
+    return mongo
+   
+
+def write_object(log_file, wixel_data):
+    mongo = create_object(wixel_data)
+
+    client = MongoClient(db_uri+ '/'+db_name + '?socketTimeoutMS=180000&connectTimeoutMS=60000')
+    db = client[db_name]
+    collection = db['SnirData']
+    post_id = collection.insert_one(mongo).inserted_id
+    log(log_file, "succesfully uploaded object to mongo")
+
+def log(file, string):
+    i = datetime.datetime.now()
+    now = "%s" %i 
+    print (now[:-3]+ ':  ' +string)
+    file.write(now[:-3]+ ':  ' + string)
+    file.write('\r\n')
+
+
+def comport_loop(log_file, port_name):
+    ser = serial.Serial(port_name, 9600, timeout=0)
+    log_file.write('Starting loop\r\n')
+    while 1:
+        try:
+            bytes = ser.read(ser.inWaiting())
+            if len(bytes) < 1:
+                time.sleep(50.0/1000.0)
+                continue
+            print(bytes)
+            data = bytes.decode("ASCII")
+
+            #if not data:
+            #  time.sleep(50.0/1000.0)
+            #  continue
+        
+            lines =data.splitlines()
+            for line in lines:
+                log(log_file, line)
+                try:
+                    write_object(log_file, line)
+                except ValueError as value_error:
+                    log(log_file, 'caught exception ' + str(value_error))
+        
+        except ser.SerialTimeoutException:
+                  print('Data could not be read')
+                  file.write('caught exception\r\n')
 
 
 if len(sys.argv) != 2:
   print ("Need to supply port")
   sys.exit()
 
-if sys.argv[1].startswith('com'):
-    # this is windows
-    port_name = sys.argv[1]
-    file = open('%s_hist.txt' % port_name , 'a', 1)
-else:
-    # this is linux
-    port_name = get_com_from_name(sys.argv[1])
-    file = open('hist-%s.txt' % sys.argv[1] , 'a', 1)
+port_name = sys.argv[1]
+log_file = open('%s_hist.txt' % port_name , 'a', 1)
+
+comport_loop(log_file, port_name)
 
 
-ser = serial.Serial(port_name, 9600, timeout=0)
-while 1:
-    try:
-        bytes = ser.read(ser.inWaiting())
-        if len(bytes) < 1:
-          time.sleep(50.0/1000.0)
-          continue
-        print(bytes)
-        data = bytes.decode("ASCII")
-
-        #if not data:
-        #  time.sleep(50.0/1000.0)
-        #  continue
-        
-        lines =data.splitlines()
-        i = datetime.datetime.now()
-        now = "%s" %i 
-        
-        for line in lines:
-          print (now[:-3]+ line)
-          file.write(now[:-3]+ line)
-          file.write('\r\n')
-        
-    except ser.SerialTimeoutException:
-        print('Data could not be read')
     
