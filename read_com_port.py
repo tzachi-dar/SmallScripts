4ffc5e5f386cdb59b881bcdb5d8c1f290c598928
Tzachi Dar <tzachi.dar@gmail.com>
2017-03-26 10:01:21 +0300

    Add code for using sqllite3.
    Signed-off-by: Tzachi Dar <tzachi.dar@gmail.com>

--- read_com_port.py
+++ read_com_port.py
@@ -5,10 +5,108 @@ import time
 import sys
 import socket
 from pymongo import MongoClient
+import sqlite3
+
 
 db_uri = 'mongodb://user:password@ds041673.mlab.com:41673'
 db_name = 'nightscout'
 transmitter_id = '6FNTM'
+
+
+''' ------------------------ sqllite3 functions ---------------------------'''
+
+class sqllite3_wrapper:
+
+    file_name = 'example.db'
+
+    def CreateTable(self):
+        conn = sqlite3.connect(self.file_name)
+        cursor=conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS G4Readings (
+                                    TransmitterId text NOT NULL,
+                                    RawValue integer NOT NULL,
+                                    FilteredValue integer NOT NULL,
+                                    BatteryLife integer,
+                                    ReceivedSignalStrength integer,
+                                    TransmissionId integer,
+                                    UploaderBatteryLife integer,
+                                    CaptureDateTime BIGINT,
+                                    DebugInfo text NOT NULL,
+                                    Uploaded int,
+                                    PRIMARY KEY (CaptureDateTime, DebugInfo))''')
+        conn.commit()
+        conn.close()
+
+    def InsertReading(self, reading):
+        #expects a dict like the one created in create_object
+        conn = sqlite3.connect(self.file_name)
+        with conn:
+            cursor=conn.cursor()
+            cursor.execute("INSERT INTO G4Readings  values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                (reading['TransmitterId'], 
+                 reading['RawValue'],
+                 reading['FilteredValue'],
+                 reading['BatteryLife'],
+                 reading['ReceivedSignalStrength'],
+                 reading['TransmissionId'],
+                 reading['UploaderBatteryLife'],
+                 reading['CaptureDateTime'],
+                 reading['DebugInfo'],
+                 reading['Uploaded']))
+             
+
+    def GetLatestNotUploadedObjects(self, count):
+        # gets the latest n non commited objects
+        ret = []
+        conn = sqlite3.connect(self.file_name)
+        with conn:
+            cursor = conn.execute("SELECT * FROM G4Readings WHERE Uploaded=:Uploaded ORDER BY CaptureDateTime DESC LIMIT :Limit", 
+                 {"Uploaded": 0, "Limit": count})
+        
+        
+            for raw in cursor:
+                raw_dict = dict()
+                raw_dict['TransmitterId'] = raw[0]
+                raw_dict['RawValue'] = raw[1]
+                raw_dict['FilteredValue'] = raw[2]
+                raw_dict['BatteryLife'] = raw[3]
+                raw_dict['ReceivedSignalStrength'] = raw[4]
+                raw_dict['TransmissionId'] = raw[5]
+                raw_dict['UploaderBatteryLife'] = raw[6]
+                raw_dict['CaptureDateTime'] = raw[7]
+                raw_dict['DebugInfo'] = raw[8]
+                raw_dict['Uploaded'] = raw[9]
+                ret.insert(0,raw_dict)
+                print(raw)
+        for raw in ret:
+            print(raw)
+            print (type (raw))
+        return ret
+    
+    def UpdateUploaded(self, CaptureDateTime, DebugInfo):
+        conn = sqlite3.connect(self.file_name)
+        with conn:
+            cur = conn.cursor()    
+            cur.execute("UPDATE G4Readings SET Uploaded=? WHERE CaptureDateTime=? and DebugInfo=?", (1, CaptureDateTime, DebugInfo))        
+            conn.commit()
+            print ("Number of rows updated: %d" % cur.rowcount)
+
+    def RunLocalTests(self):
+        sqw = sqllite3_wrapper ()
+        sqw.CreateTable()
+         for i in range(1, 10000):
+         if not i % 1000: print(i)
+         obj = create_object('port1', '6ABW4 54880 44800 213 -89 2')
+         sqw.InsertReading(obj)
+
+         print("before get")
+         lastones = sqw.GetLatestNotUploadedObjects(5)
+         for ob in lastones:
+             sqw.UpdateUploaded(ob['CaptureDateTime'], ob['DebugInfo'])
+         sys.exit(0)
+
+''' ------------------------ end of sqllite3 functions ---------------------------'''
+
 
 def create_object(port_name, wixel_data):
     # an example to wixel_data is '6ABW4 54880 44800 213 -89 2'
@@ -24,6 +122,7 @@ def create_object(port_name, wixel_data):
     captured_time = int(time.time() * 1000)
     mongo['CaptureDateTime'] = captured_time
     mongo['DebugInfo'] = '%s %s %s' % (socket.gethostname(), time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(captured_time / 1000)), port_name)
+    mongo['Uploaded'] = 0
 
     return mongo
 
