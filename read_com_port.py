e12ec263d0295114ad2e9149dcf92d460141b6fe
Tzachi Dar <tzachi.dar@gmail.com>
2017-03-27 19:33:27 +0300

    Add a listner socket, and handle json requests.

--- read_com_port.py
+++ read_com_port.py
@@ -9,11 +9,36 @@ import sqlite3
 import os
 import inspect
 import threading
+import json
+import signal
 
 db_uri = 'mongodb://user:password@ds041673.mlab.com:41673'
 db_name = 'nightscout'
 transmitter_id = '6FNTM'
 
+HOST = ''  # All interfaces
+PORT = 50005  # xdrip standard port
+
+def create_object(port_name, wixel_data):
+    # an example to wixel_data is '6FNTM 54880 44800 213 -89 2'
+    mongo = dict()
+    data = wixel_data.split()
+    mongo['TransmitterId'] = data[0]
+    if mongo['TransmitterId'] != transmitter_id:
+        log(log_file, "rejecting packet with transmision id %s" % mongo['TransmitterId'])
+        return None
+    mongo['RawValue'] = int(data[1])
+    mongo['FilteredValue'] = int(data[2])
+    mongo['BatteryLife'] = int(data[3])
+    mongo['ReceivedSignalStrength'] = int(data[4])
+    mongo['TransmissionId'] = int(data[5])
+    mongo['UploaderBatteryLife'] = 100
+    captured_time = int(time.time() * 1000)
+    mongo['CaptureDateTime'] = captured_time
+    mongo['DebugInfo'] = '%s %s %s' % (socket.gethostname(), time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(captured_time / 1000)), port_name)
+    mongo['Uploaded'] = 0
+
+    return mongo
 
 ''' ------------------------ sqllite3 functions ---------------------------'''
 
@@ -59,14 +84,17 @@ class sqllite3_wrapper:
                  reading['Uploaded']))
              
 
-    def GetLatestNotUploadedObjects(self, count):
+    def GetLatestObjects(self, count, only_not_uploaded):
         # gets the latest n non commited objects
         ret = []
         conn = sqlite3.connect(self.file_name)
         with conn:
-            cursor = conn.execute("SELECT * FROM G4Readings WHERE Uploaded=:Uploaded ORDER BY CaptureDateTime DESC LIMIT :Limit", 
-                 {"Uploaded": 0, "Limit": count})
-        
+            if only_not_uploaded:
+                cursor = conn.execute("SELECT * FROM G4Readings WHERE Uploaded=:Uploaded ORDER BY CaptureDateTime DESC LIMIT :Limit", 
+                    {"Uploaded": 0, "Limit": count})
+            else:
+                cursor = conn.execute("SELECT * FROM G4Readings ORDER BY CaptureDateTime DESC LIMIT :Limit", 
+                    {"Limit": count})
         
             for raw in cursor:
                 raw_dict = dict()
@@ -98,42 +126,87 @@ class sqllite3_wrapper:
     def RunLocalTests(self):
         sqw = sqllite3_wrapper ()
         sqw.CreateTable()
-        for i in range(1, 100000):
+        for i in range(1, 1000):
             if not i % 1000: print(i)
-            obj = create_object('port1', '6ABW4 54880 44800 213 -89 2')
-            #sqw.InsertReading(obj)
+            obj = create_object('port1', '6FNTM 54880 44800 213 -89 2')
+            sqw.InsertReading(obj)
 
         print("before get")
-        lastones = sqw.GetLatestNotUploadedObjects(5)
+        lastones = sqw.GetLatestObjects(5, False)
         for ob in lastones:
             sqw.UpdateUploaded(ob['CaptureDateTime'], ob['DebugInfo'])
         sys.exit(0)
 
+#sqw = sqllite3_wrapper ()
+#sqw.RunLocalTests()
+
+''' ----------------- threads that respond to tcp requests -----------------------'''
+
+def clientThread(connlocal):
+    try:
+        connlocal.settimeout(10)
+        while True:
+            data = connlocal.recv(1024)
+            reply = ''
+            if not data:
+                break
+            decoded = json.loads(data)
+                        print("type decpded = %s" % type (decoded))
+            print json.dumps(decoded, sort_keys=True, indent=4)
+                        if decoded['version'] != 1:
+                            print("bad version %s" % decoded['version'] )
+
+                        sqw = sqllite3_wrapper()
+                        not_uploaded_readings = sqw.GetLatestObjects( decoded['numberOfRecords'],False)
+                        for reading_dict in not_uploaded_readings:
+                           reading_dict['RelativeTime'] = (int(time.time()*1000) ) - reading_dict['CaptureDateTime']
+                           if reading_dict['RelativeTime'] < 0:
+                               continue
+                           reply = reply + json.dumps(reading_dict) +"\n"
+
+            print ("reply = %s" % reply)
+                        reply = reply + "\n"
+
+            connlocal.sendall(reply)
+        connlocal.close()
+
+    except Exception, e:
+        print "Exception in clientThread: ", e
+
+def CreateListeningSocket():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    print 'Socket created'
+
+    # Bind socket to local host and port
+
+    try:
+        s.bind((HOST, PORT))
+    except socket.error as msg:
+        print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+        return
+
+    s.listen(10)
+    print "Waiting for connections"
+
+    while 1:
+        conn, addr = s.accept()
+        print 'Connected with ' + addr[0] + ':' + str(addr[1])
+
+        threading.Thread(target=clientThread, args=(conn,)).start()
+
+def CreateListeningSocketWrapper():
+    while 1:
+        try:
+            CreateListeningSocket()
+        except Exception as exception :  
+            #???? log(log_file, 'CreateListeningSocketWrapper caught exception in while loop' + str(exception) + exception.__class__.__name__)
+            time.sleep(60)
 
 
-''' ------------------------ end of sqllite3 functions ---------------------------'''
+''' ------------------------------------------------------------------------------'''
 
-
-def create_object(port_name, wixel_data):
-    # an example to wixel_data is '6FNTM 54880 44800 213 -89 2'
-    mongo = dict()
-    data = wixel_data.split()
-    mongo['TransmitterId'] = data[0]
-    if mongo['TransmitterId'] != transmitter_id:
-        log(log_file, "rejecting packet with transmision id %s" % mongo['TransmitterId'])
-        return None
-    mongo['RawValue'] = int(data[1])
-    mongo['FilteredValue'] = int(data[2])
-    mongo['BatteryLife'] = int(data[3])
-    mongo['ReceivedSignalStrength'] = int(data[4])
-    mongo['TransmissionId'] = int(data[5])
-    mongo['UploaderBatteryLife'] = 100
-    captured_time = int(time.time() * 1000)
-    mongo['CaptureDateTime'] = captured_time
-    mongo['DebugInfo'] = '%s %s %s' % (socket.gethostname(), time.strftime('%d-%m-%Y %H:%M:%S', time.localtime(captured_time / 1000)), port_name)
-    mongo['Uploaded'] = 0
-
-    return mongo
 
 def write_log_to_mongo(log_file, port_name, log_message):
     mongo = dict()
@@ -225,7 +298,7 @@ class MongoWrapper(threading.Thread):
                 # The next line introduces many races that are only fixed by the timeout on wait.
                 self.event.clear()
                 sqw = sqllite3_wrapper ()
-                not_uploaded_readings = sqw.GetLatestNotUploadedObjects(12 * 8)
+                not_uploaded_readings = sqw.GetLatestObjects(12 * 8, True)
                 for reading_dict in not_uploaded_readings:
                     write_object_to_mongo(log_file, reading_dict)
                     sqw.UpdateUploaded(reading_dict['CaptureDateTime'], reading_dict['DebugInfo'])
@@ -244,6 +317,11 @@ log_file = open('%s_hist.txt' % port_name , 'a', 1)
 # sleep for 30 seconds to let the system connect to the network
 time.sleep(30)
 
+#Create the sqllite table.
+sqw = sqllite3_wrapper ()
+sqw.CreateTable()
+
+
 try:
     write_log_to_mongo(log_file, port_name, "starting program")
 except Exception as exception :  
@@ -255,11 +333,14 @@ try:
 except Exception as exception :  
     # This is a critical failure, we will continue going up, but in a very bad state. Consider quiting the program
     log(log_file, 'WTF, caught exception in MongoWrapper cration ' + str(exception) + exception.__class__.__name__)
-    
-#Create the sqllite table.
-sqw = sqllite3_wrapper ()
-sqw.CreateTable()
 
+    
+# start the listner thread
+try:
+    threading.Thread(target= CreateListeningSocketWrapper).start()
+except Exception as exception :  
+    # This is a critical failure, we will continue going up, but in a very bad state. Consider quiting the program
+    log(log_file, 'WTF, caught exception in opening listening socket ' + str(exception) + exception.__class__.__name__)
 
 
 while 1:
@@ -267,6 +348,7 @@ while 1:
         comport_loop(log_file, port_name, mongo_wrapper)
     except KeyboardInterrupt as keyboardInterrupt :
         log(log_file, 'caught exception KeyboardInterrupt:' + str(keyboardInterrupt))
+    os.kill(os.getpid(), signal.SIGKILL)
         sys.exit(0)
     except Exception as exception :  
         log(log_file, 'caught exception in while loop' + str(exception) + exception.__class__.__name__)
